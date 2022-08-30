62d0b6f06ea05e437b5571b5370bab06c22f74e7
Nataliia Lazorenko <macbook@MacBook-Pro-Nataliia.local>
2022-08-30 22:06:16 +0300

    Test and code button click

--- src/App.test.js
+++ src/App.test.js
@@ -1,57 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-/*
- * global test method has two arguments:
- * - string description
- * - test function
- * Test fails if error is thrown when running function
- * - assertions throw errors when expectation fails
- * No error -> tests pass
- * - Empty test passes! - it doesn't matter if there is nothing in the test. As long as no error is thrown, the test will pass
- 
- * render
- * - Create virtual DOM for argument JSX
- * - Access virtual DOM via screen global object
- */
-
-test("renders learn react link", () => {
+test("button has correct initial color", () => {
   render(<App />);
-  /* 
-   * screen.getByText()
-   * - Find element by display text
 
-   * /learn react/i
-   * - regular expression
-   * - case insentitive (i) - all the letters can be either uppercase or lowercase
-   * - could be string 'Learn React' - if we don't want to use a regular expression, we are more than welcome to use an actual string
-   
-   * expect().toBeInTheDocument()
-   * - assertion (твердження), causes test to succeed or fail
-   */
+  // find an element with a role and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" }); // screen - global object that has access to the virtual DOM, created by render
 
-  /*
-   * getByRole has 2 arguments:
-   * - the role itself ('a' element has a builden role of 'link')
-   * - options(we will use 'name' option to identify what the displayed text should be for this link)
-   */
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
-  /*
-   * expect
-   * - Jest global method, starts the assertion
-   
-   * expect argument (here linkElement)
-   * - subject of the assertion - this is what jest will examining to see if it meets our expectations
+  // click button
+  fireEvent.click(colorButton); // fireEvent - helps to interact the elements in Virtual DOM; click method takes an element
 
-   * matcher (відповідник) (here toBeInTheDocument)
-   * - type of assertion
-   * - toBeInTheDocument - this matcher comes from Jest-DOM
-   
-   * matcher argument - sometimes there is an argument to the matcher
-   * - refines matcher
-   
-   * toBeInTheDocument - doesn't have an argument (the element is in document or not)
-   */
-  expect(linkElement).toBeInTheDocument();
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // the specs also specify, that the button should have the correct text after the background color has changed, so we test this too
+  // expect the button text to be "Change to red"
+  expect(colorButton.textContent).toBe("Change to red");
 });
